442741d7dba3cc86574efd07eb2dc3bef2c34744
fly <503638407@qq.com>
2019-09-15 17:43:15 +0800

    01初始化了vuepress项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title:"vue-router-note",
+    themeConfig:{
+        sidebar:[
+            ["/",'简介'],
+            ["/pages/grammer/grammer",'markdown语法'],
+            ['/pages/VueRouter/router','学习笔记']
+        ]
+    }
+}
